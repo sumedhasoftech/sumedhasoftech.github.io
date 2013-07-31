e257da13fbde9d19fd1430b78660a93988d9b0df
manojmeena <manojmeena1988@gmail.com>
2013-07-31 20:55:35 +0530

    template created

---
+++ jscript/main.js
@@ -0,0 +1,22 @@
+
+
+
+$(window).load(function() {
+	
+	// activate the loading image on window load
+		$(".loading").fadeOut(300); 
+		
+		// activate the alerts on window load
+		$(".alert").show(800);	
+});
+
+$(document).ready(function(){
+
+	
+
+	// close the alerts on click of close button
+	$(".close").click(function(){
+		$(this).closest(".alert").hide(500);	
+	})
+	
+});
